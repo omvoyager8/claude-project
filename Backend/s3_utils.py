2cac98e980c8b3a7a2009827f92015a737064e21
omvoyager8 <omgonjari1978@gmail.com>
2026-05-08 12:52:18 +0530

    hosting is on

--- Backend/s3_utils.py
+++ Backend/s3_utils.py
@@ -1,8 +1,14 @@
 import boto3
-from config import AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET
+import os
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+AWS_REGION = os.getenv("AWS_REGION")
 
 s3 = boto3.client(
     "s3",
+    region_name=AWS_REGION,
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY
 )
